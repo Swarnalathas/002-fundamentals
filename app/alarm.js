db1477bc24c7a92332d7ccbc65de32efa022a6f9
Swarna <swarna.prakashm@gmail.com>
2019-03-20 23:24:07 +0000

    Completed tasks 002

--- app/alarm.js
+++ app/alarm.js
@@ -6,29 +6,113 @@ const peopleThatHaveUsedAlarm = [
   { name: "Lisa", wakeUpTime: 6 }
 ];
 
+function alaramUsed(){
+
+  let alaramUserName = [];
+ // peopleThatHaveUsedAlarm.forEach(function(people){
+   peopleThatHaveUsedAlarm.map(function(people){
+    alaramUserName.push(people.name);
+  });
+
+  return alaramUserName;
+}
+
+function peopleWakeUpTimes () {
+
+  let useWakeUpTime = [];
+  peopleThatHaveUsedAlarm.map(function(people){
+    let wakeTime = people.wakeUpTime + 1;
+    useWakeUpTime.push(people.name + " wakes up at " + covertTimeToAmPm(wakeTime));
+  });
+  return useWakeUpTime;
+}
+
+function covertTimeToAmPm(wakeTime){
+
+  return (wakeTime > 12 ? wakeTime -12 : wakeTime) 
+  + (wakeTime < 12 ? 'am' : 'pm')
+  
+}
+
+function wakeupEarlyPay (time){
+
+  let wakeupEarlyUsers = [];
+  peopleThatHaveUsedAlarm.filter(function(people){
+    if (people.wakeUpTime <= time)  {
+      
+      wakeupEarlyUsers.push(people);
+    }
+  });
+  return wakeupEarlyUsers;
+}
+
 function wakeup(currentTime) {
   // currentTime will be a Javascript Date object
   // https://www.w3schools.com/jsref/jsref_obj_date.asp
   const currentHour = currentTime.getHours();
 
-  if (currentHour > 12) {
+ /* if (currentHour < 12) {
     return "Ugggggh its too early - I'm not waking up yet";
+  } else if (currentHour >= 12 && currentHour < 14){
+
+    return "Yessssss its lunch time!!!";
   } else {
     return "Get up!! You're wasting the day";
+  }*/
+
+  let msg;
+  switch (currentHour) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+      msg= "Ugggggh its too early - I'm not waking up yet";
+      break;
+    case 12:
+    case 13:
+    case 14:
+      msg= "Yessssss its lunch time!!!";
+      break;
+    default:
+      msg = "Get up!! You're wasting the day";
+      break;
   }
+  return msg;
 }
 
-function snooze() {
+/*function snooze() {
   let snoozeAttempts = 0;
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 20; i++) {
     snoozeAttempts++;
   }
 
   return snoozeAttempts;
+}*/
+
+function snooze(){
+  let snoozeAttempts = 0;
+  while(snoozeAttempts < 20){
+      snoozeAttempts++;
+  }
+  return snoozeAttempts;
 }
+
 
 module.exports = {
   wakeup,
-  snooze
+  snooze,
+  alaramUsed,
+  peopleWakeUpTimes,
+  covertTimeToAmPm,
+  wakeupEarlyPay
+
+
 };
--- test/alarm.test.js
+++ test/alarm.test.js
@@ -2,10 +2,48 @@ const alarm = require('../app/alarm');
 
 test('Moans if too early', () => {
   let timeNow = new Date();
-  timeNow.setHours(11);
+  timeNow.setHours(9);
   expect(alarm.wakeup(timeNow)).toBe("Ugggggh its too early - I'm not waking up yet");
 });
 
+test('CurrentHour is LunchTime', () => {
+  let timeNow = new Date();
+  timeNow.setHours(13);
+  expect(alarm.wakeup(timeNow)).toBe("Yessssss its lunch time!!!");
+});
+
+test('CurrentHour is After LunchTime', () => {
+  let timeNow = new Date();
+  timeNow.setHours(15);
+  expect(alarm.wakeup(timeNow)).toBe("Get up!! You're wasting the day");
+});
+
+test('People Who used Alaram', () => {
+  const alaramUsedUser =  ["James","Susan","Geoff","Lisa"];
+  expect(alarm.alaramUsed()).toEqual(alaramUsedUser);
+});
+
+test('People Who used Alaram and Add 1 Hour Time', () => {
+  const alaramUsedUser =  ["James wakes up at 8am","Susan wakes up at 2pm",
+  "Geoff wakes up at 3pm","Lisa wakes up at 7am"];
+  expect(alarm.peopleWakeUpTimes()).toEqual(alaramUsedUser);
+});
+
+test('People wakes up the before 9 get increased pay' ,() => {
+   let wakeupEarlyPay = [ { name: "James", wakeUpTime: 7 } , {name: "Lisa", wakeUpTime: 6 }];
+  expect(alarm.wakeupEarlyPay(9)).toEqual(wakeupEarlyPay);
+
+});
+
+test('CEo Change Minds' ,() => {
+  let wakeupEarlyPay = [ { name: "James", wakeUpTime: 7 },
+  { name: "Susan", wakeUpTime: 13 },
+  { name: "Geoff", wakeUpTime: 14 },
+  { name: "Lisa", wakeUpTime: 6 }];
+ expect(alarm.wakeupEarlyPay(14)).toEqual(wakeupEarlyPay);
+
+});
+
 test('Snoozing for a set amount', () => {
-  expect(alarm.snooze()).toBe(10);
+  expect(alarm.snooze()).toBe(20);
 });
